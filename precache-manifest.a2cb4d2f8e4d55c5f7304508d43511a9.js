92cc3f388957df594dedfdc895576a73019fa555
unknown <perez.alejandro1221@gmail.com>
2022-12-01 15:18:07 -0400

    Updates

---
+++ precache-manifest.a2cb4d2f8e4d55c5f7304508d43511a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a0b47a536cff21434a4",
+    "url": "/enstock/static/css/main.ab6dd7c2.chunk.css"
+  },
+  {
+    "revision": "0a0b47a536cff21434a4",
+    "url": "/enstock/static/js/main.0a0b47a5.chunk.js"
+  },
+  {
+    "revision": "7f1662d5cca94d6900a5",
+    "url": "/enstock/static/js/1.7f1662d5.chunk.js"
+  },
+  {
+    "revision": "77be7a7fdb72600f3cde",
+    "url": "/enstock/static/js/2.77be7a7f.chunk.js"
+  },
+  {
+    "revision": "dd708c937da0d0e9f061",
+    "url": "/enstock/static/js/runtime~main.dd708c93.js"
+  },
+  {
+    "revision": "ad5f5c2799ae3f4df62bdb2ee333e6fd",
+    "url": "/enstock/index.html"
+  }
+];
